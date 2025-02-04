12784818b1c3d1b201add61062c4bc6e5be8b5f6
José Padilla <jpadilla.isc@gmail.com>
2025-02-04 16:16:07 +0100

    script terminado, antes de probar

--- keepcoding.sql
+++ keepcoding.sql
@@ -34,5 +34,37 @@ CREATE TABLE Alumnos (
 	name VARCHAR(80) NOT NULL,
 	first_name VARCHAR(80) NOT NULL,
 	status status_alumno NOT NULL,
-	
+	phone VARCHAR(12) NOT NULL,
+	email VARCHAR(150) NOT NULL,
+	linkedin VARCHAR(255),
+	github VARCHAR(255)
+);
+
+---- Relaciones N a N
+-- Creación tabla Modulo_bootcamp
+CREATE TABLE Modulo_bootcamp (
+	profesor_id INT NOT NULL,
+	bootcamp_id INT NOT NULL,
+	edition INT NOT NULL,
+	start_date date,
+	finsh_date date,
+	exam_date date,
+	module_link VARCHAR(255)
+	FOREIGN KEY (profesor_id) REFERENCES Profesores(id),
+	FOREIGN KEY (bootcamp_id) REFERENCES Bootcamp(id),
+	PRIMARY KEY(profesor_id, bootcamp_id, edition)
+);
+
+-- Creación tabla Alumno_Bootcamp
+CREATE TYPE status_suscripcion AS ENUM('interesded', 'pending interview', 'accepted', 'denided', 'payed', 'finished satisfactory', 'finished complete', 'finished not complete', 'cancelled');
+CREATE TABLE Alumno_Bootcamp (
+	bootcamp_id INT NOT NULL,
+	alumno_id INT NOT NULL,
+	edition INT NOT NULL,
+	status status_suscripcion DEFAULT 'interesded',
+	suscription_date date,
+	notes VARCHAR(255),
+	FOREIGN KEY (bootcamp_id) REFERENCES Bootcamp(id),
+	FOREIGN KEY (alumno_id) REFERENCES Alumnos(id),
+	PRIMARY KEY(profesor_id, bootcamp_id, edition)
 );
